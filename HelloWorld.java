71adcbab2f82c13a7913f8d12db14fde23b97cb9
iNekox3 <inekox3@hotmail.com>
2017-05-05 15:29:45 +0800

    New HelloWorld.java file
    Add a new java file containing a main and run method.

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HelloWorld {
+	public void run() {
+		
+	}
+
+	public static void main(String[] args) {
+		HelloWorld MyWorld = new HelloWorld();
+		MyWorld.run();
+	}
+}
